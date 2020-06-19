4920173344c994f0f00d5ec65228b1b98c6610e0
Kyle Nakamura <kjnakamura@apu.edu>
2020-06-19 16:02:35 -0700

    Implement ObjectPooling to fix memory leak

--- Assets/ImageSynthesis/ImageSynthesis.cs
+++ Assets/ImageSynthesis/ImageSynthesis.cs
@@ -156,7 +156,7 @@ public class ImageSynthesis : MonoBehaviour {
         }
     }
 
-    public void Save (string filename, int width = -1, int height = -1, string path = "") {
+    public void Save (string filename, int width = -1, int height = -1, string path = "", int specificPass = -1) {
         if (width <= 0 || height <= 0) {
             width = Screen.width;
             height = Screen.height;
@@ -171,17 +171,24 @@ public class ImageSynthesis : MonoBehaviour {
 
         // execute as coroutine to wait for the EndOfFrame before starting capture
         StartCoroutine (
-            WaitForEndOfFrameAndSave (pathWithoutExtension, filenameExtension, width, height));
+            WaitForEndOfFrameAndSave (pathWithoutExtension, filenameExtension, width, height, specificPass));
     }
 
-    private IEnumerator WaitForEndOfFrameAndSave (string filenameWithoutExtension, string filenameExtension, int width, int height) {
+    private IEnumerator WaitForEndOfFrameAndSave (string filenameWithoutExtension, string filenameExtension, int width, int height, int specificPass) {
         yield return new WaitForEndOfFrame ();
-        Save (filenameWithoutExtension, filenameExtension, width, height);
+        Save (filenameWithoutExtension, filenameExtension, width, height, specificPass);
     }
 
-    private void Save (string filenameWithoutExtension, string filenameExtension, int width, int height) {
-        foreach (var pass in capturePasses)
+    private void Save (string filenameWithoutExtension, string filenameExtension, int width, int height, int specificPass) {
+        if (specificPass == -1) {
+            foreach (var pass in capturePasses)
+                Save (pass.camera, filenameWithoutExtension + pass.name + filenameExtension, width, height, pass.supportsAntialiasing, pass.needsRescale);
+        } else {
+            var pass = capturePasses[0];
             Save (pass.camera, filenameWithoutExtension + pass.name + filenameExtension, width, height, pass.supportsAntialiasing, pass.needsRescale);
+            pass = capturePasses[specificPass];
+            Save (pass.camera, filenameWithoutExtension + pass.name + filenameExtension, width, height, pass.supportsAntialiasing, pass.needsRescale);
+        }
     }
 
     private void Save (Camera cam, string filename, int width, int height, bool supportsAntialiasing, bool needsRescale) {
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,23 +6,29 @@ using UnityEngine;
 public class SceneController: MonoBehaviour {
     public ImageSynthesis synth;
     public GameObject[] prefabs;
+    public int minObjects;
     public int maxObjects;
 
-    private GameObject[] created;
+    private ShapePool pool;
+    private int frameCount = 0;
 
     /********************
      * Built in methods *
      ********************/
 
-    // Start is called before the first frame update
     void Start () {
-        created = new GameObject[maxObjects];
+        pool = ShapePool.Create(prefabs);
     }
 
-    // OnRenderObject is called after all regular scene rendering is done
-    void OnRenderObject() {
-        GenerateRandomShapes();
-        synth.OnSceneChange();
+    void Update() {
+        if (frameCount % 30 == 0 ) {
+            GenerateRandomShapes();
+        }
+        frameCount++;
+
+        string dir = "/Users/kjnakamura/local-documents/local-development/ml-synth-captures";
+        string fileName = $"image_{frameCount.ToString().PadLeft(5, '0')}";
+        synth.Save(fileName, 512, 512, dir, 2);
     }
 
     /******************
@@ -31,17 +37,14 @@ public class SceneController: MonoBehaviour {
 
     void GenerateRandomShapes() {
         // Destroy existing objects before creating new ones
-        foreach (GameObject obj in created) {
-            if (obj != null) {
-                Destroy(obj);
-            }
-        }
+        pool.ReclaimAll();
+
+        int objectsThisTime = Random.Range(minObjects, maxObjects);
 
         // Create objects with random properties
-        for (int i = 0; i < maxObjects; i++) {
-            // Pick a random prefab (shape)
+        for (int i = 0; i < objectsThisTime; i++) {
+            // Pick a random prefab
             int prefabIndex = Random.Range(0, prefabs.Length);
-            GameObject prefab = prefabs[prefabIndex];
 
             // Set position
             float newX, newY, newZ;
@@ -53,22 +56,26 @@ public class SceneController: MonoBehaviour {
             // Set rotation
             Quaternion newRot = Random.rotation;
 
-            // Place the prefab in the scene
-            GameObject newObj = Instantiate(prefab, newPos, newRot);
-            created[i] = newObj;
-
-            // Set scale
-            float s = Random.Range(0.5f, 4.0f);
-            Vector3 newScale = new Vector3(s, s, s);
-            newObj.transform.localScale = newScale;
-
             // Set color
             float newR, newG, newB;
             newR = Random.Range(0.0f, 1.0f);
             newG = Random.Range(0.0f, 1.0f);
             newB = Random.Range(0.0f, 1.0f);
             Color newColor = new Color(newR, newG, newB);
+
+            // Set scale
+            float s = Random.Range(0.5f, 4.0f);
+            Vector3 newScale = new Vector3(s, s, s);
+
+            // Place the prefab in the scene with its new properties
+            Shape shape = pool.Get((ShapeLabel) prefabIndex);
+            GameObject newObj = shape.obj;
+            newObj.transform.position = newPos;
+            newObj.transform.localScale = newScale;
+            newObj.transform.localScale = newScale;
             newObj.GetComponent<Renderer>().material.color = newColor;
         }
+
+        synth.OnSceneChange();
     }
 }
